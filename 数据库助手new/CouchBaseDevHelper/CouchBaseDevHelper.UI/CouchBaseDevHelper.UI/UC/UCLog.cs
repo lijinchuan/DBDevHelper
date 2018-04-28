a63a77dbf602b7c7841f4bf669ad6a086d6efacd
lijinchuan <403479851@qq.com>
2018-04-28 23:14:08 +0800

    fix bug:修改日志数据不刷新

--- 数据库助手new/CouchBaseDevHelper/CouchBaseDevHelper.UI/CouchBaseDevHelper.UI/UC/UCLog.cs
+++ 数据库助手new/CouchBaseDevHelper/CouchBaseDevHelper.UI/CouchBaseDevHelper.UI/UC/UCLog.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using LJC.FrameWork.Data.EntityDataBase;
+using System.Threading;
 
 namespace CouchBaseDevHelper.UI.UC
 {
@@ -101,6 +102,8 @@ namespace CouchBaseDevHelper.UI.UC
                          {
                              log.Mark = inputform.Val;
                              EntityTableEngine.LocalEngine.Update<SearchLog>(Global.TBName_SearchLog, log);
+                             this.gvlog.ClearSelection();
+                             Thread.Sleep(3000);
                              this.LoadLog();
                          }
                      }
