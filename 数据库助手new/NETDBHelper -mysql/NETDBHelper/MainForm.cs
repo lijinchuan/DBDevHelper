5df8d7aa60bd7b45683da353329172cd6c00ef76
li.jinchuan <jinchuan.li@onesmart.org>
2021-05-11 19:09:04 +0800

    * FIX BUG：关不了程序

--- 数据库助手new/NETDBHelper -mysql/NETDBHelper/MainForm.cs
+++ 数据库助手new/NETDBHelper -mysql/NETDBHelper/MainForm.cs
@@ -150,7 +150,10 @@ namespace NETDBHelper
                 e.Cancel = true;
                 return;
             }
-
+            if (e.Cancel)
+            {
+                e.Cancel = false;
+            }
             base.OnClosing(e);
             if (tasktimer != null)
             {
