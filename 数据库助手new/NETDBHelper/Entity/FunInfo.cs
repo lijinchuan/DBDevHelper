228324c35da6b18114990709d559eddb2a5d0a16
ljc <403479851@qq.com>
2021-06-26 13:25:48 +0800

    查找函数

---
+++ 数据库助手new/NETDBHelper/Entity/FunInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    public class FunInfo : INodeContents
+    {
+        public NodeContentType GetNodeContentType()
+        {
+            return NodeContentType.FUN;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public bool IsTableValue
+        {
+            get;
+            set;
+        }
+
+        public bool IsScalar
+        {
+            get;
+            set;
+        }
+
+        public List<FunParamInfo> FuncParamInfos
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ 数据库助手new/NETDBHelper/Entity/FunParamInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    public class FunParamInfo : INodeContents
+    {
+        public NodeContentType GetNodeContentType()
+        {
+            return NodeContentType.FUNPARAM;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int Len
+        {
+            get;
+            set;
+        }
+
+        public bool HasDefaultValue
+        {
+            get;
+            set;
+        }
+
+        public bool IsOutparam
+        {
+            get;
+            set;
+        }
+
+
+        public string TypeName
+        {
+            get;
+            set;
+        }
+
+    }
+}
