6c4e24b3c8b3138bf8db5a74d5af668dda7dc1e5
Tom <c2010@topuc.com>
2022-10-09 09:44:28 +0800

    SQL解析

---
+++ 数据库助手new/NETDBHelper/Biz/Common/SqlAnalyse/ExecAnalyser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biz.Common.SqlAnalyse
+{
+    public class ExecAnalyser : SqlAnalyser
+    {
+        private static HashSet<string> keys = new HashSet<string> { keyExec, keyExecute, keyOutput };
+
+        public override HashSet<string> GetKeys()
+        {
+            return keys;
+        }
+
+        public override string GetPrimaryKey()
+        {
+            return keyExec;
+        }
+
+        protected override bool Accept(ISqlExpress sqlExpress)
+        {
+
+            return true;
+        }
+    }
+}
---
+++ 数据库助手new/NETDBHelper/Biz/Common/SqlAnalyse/ExecuteAnalyser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biz.Common.SqlAnalyse
+{
+    public class ExecuteAnalyser:ExecAnalyser
+    {
+        public override string GetPrimaryKey()
+        {
+            return keyExecute;
+        }
+    }
+}
