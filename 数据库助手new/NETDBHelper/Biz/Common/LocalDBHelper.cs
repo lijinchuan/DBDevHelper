a0e160d21c030292bc68f6d3dcb2eba981417677
ljc <403479851@qq.com>
2022-12-17 15:22:15 +0800

    搜索优化

---
+++ 数据库助手new/NETDBHelper/Biz/Common/LocalDBHelper.cs
@@ -0,0 +1,32 @@
+﻿using Entity;
+using LJC.FrameWorkV3.Comm;
+using LJC.FrameWorkV3.Data.EntityDataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biz.Common
+{
+    public static class LocalDBHelper
+    {
+        public static List<MarkObjectInfo> GetAllMarkObjectInfoFromCach()
+        {
+            var key = "GetAllMarkObjectInfoFromCach";
+            var list=LocalCacheManager<List<MarkObjectInfo>>.FindCache(key,
+                ()=> BigEntityTableRemotingEngine.List<MarkObjectInfo>("MarkObjectInfo", 1, int.MaxValue).ToList(),
+                3);
+            return list;
+        }
+
+        public static List<SPInfo> GetAllSPInfoFromCach()
+        {
+            var key = "GetAllSPInfoFromCach";
+            var list = LocalCacheManager<List<SPInfo>>.FindCache(key,
+                () => BigEntityTableRemotingEngine.List<SPInfo>("SPInfo", 1, int.MaxValue).ToList(),
+                3);
+            return list;
+        }
+    }
+}
